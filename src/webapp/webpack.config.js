2696b97d3ab7e6d624e3623b7bc181d51bc9bad2
Chase <chase@mailbox.org>
2022-03-02 13:10:52 +0100

    move all ui code into separate folder

--- src/webapp/webpack.config.js
+++ src/webapp/webpack.config.js
@@ -7,7 +7,7 @@ const prod = mode === 'production';
 
 module.exports = {
     entry: {
-        'build/bundle': ['./src/main.ts']
+        'build/bundle': ['./src/ui/main.ts']
     },
     resolve: {
         alias: {
